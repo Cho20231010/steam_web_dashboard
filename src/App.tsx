2efcba71a065511840b50ecf1b9bc4bd95a25042
cho2310 <120286191+choco333@users.noreply.github.com>
2026-04-27 20:12:45 +0900

    Initial React TypeScript project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Steam 데이터 기반 PC 게임 시장 동향 분석 시스템</h1>
+      <p>Steam 게임 데이터를 분석하고 시각화하는 웹 대시보드입니다.</p>
+    </main>
+  );
+}
+
+export default App
